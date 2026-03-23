cbebf5f7ebaa8332e0d4b174a7397135f3c455ae
ThurzinKXZ <antonioarthur933@gmail.com>
2026-03-23 19:32:57 -0300

    Add files via upload

---
+++ Contato03/ProjetoMafra03/src/test/java/br/edu/uninassau/World/ProjetoMafra03ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.uninassau.World;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMafra03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
